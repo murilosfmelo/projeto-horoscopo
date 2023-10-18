a5b1bd814239d40ce0cfc945c781905537df354e
LEGENDARY1209 <130669909+LEGENDARY1209@users.noreply.github.com>
2023-10-18 13:34:36 -0300

    Atividade 18/10/23 arrumei string "Dia" para "Dia" int

--- atividade.py
+++ atividade.py
@@ -1,41 +1,41 @@
 #string para int
 dia = int(input("Dia: "))
-mes = input("Mes: ")
+mes = int(input("Mes: "))
 
-if dia >= 21 and mes == "3" or dia <= 19 and mes == "4":
+if dia >= 21 and mes == 3 or dia <= 19 and mes == 4:
     print("Aries")
 else:
-    if dia >= 20 and mes == "4" or dia <= 20 and mes == "5":
+    if dia >= 20 and mes == 4 or dia <= 20 and mes == 5:
         print("Touro")
     else:
-        if dia >= 21 and mes == "5" or dia <= 20 and mes == "6":
+        if dia >= 21 and mes == 5 or dia <= 20 and mes == 6:
             print("Gêmeos")
         else:
-            if dia >= 22 and mes == "6" or dia <= 22 and mes == "7":
+            if dia >= 22 and mes == 6 or dia <= 22 and mes == 7:
                 print("Cancer")
             else:
-                if dia >= 23 and mes == "7" or dia <= 22 and mes == "8":
+                if dia >= 23 and mes == 7 or dia <= 22 and mes == 8:
                     print("Leão")
                 else:
-                    if dia >= 23 and mes == "8" or dia <= 22 and mes == "9":
+                    if dia >= 23 and mes == 8 or dia <= 22 and mes == 9:
                         print("Virgem")
                     else:
-                        if dia >= 23 and mes == "9" or dia <= 22 and mes == "10":
+                        if dia >= 23 and mes == 9 or dia <= 22 and mes == 10:
                             print("Libra")
                         else:
-                            if dia >= 23 and mes == "10" or dia <= 21 and mes == "11":
+                            if dia >= 23 and mes == 10 or dia <= 21 and mes == 11:
                                 print("Escorpião")
                             else:
-                                if dia >= 22 and mes == "11" or dia <= 21 and mes == "12":
+                                if dia >= 22 and mes == 11 or dia <= 21 and mes == 12:
                                     print("Sagitário")
                                 else:
-                                    if dia >= 22 and mes == "12" or dia <= 19 and mes == "1":
+                                    if dia >= 22 and mes == 12 or dia <= 19 and mes == 1:
                                         print("Capricornio")
                                     else:
-                                        if dia >= 20 and mes == "1" or dia <= 18 and mes == "2":
+                                        if dia >= 20 and mes == 1 or dia <= 18 and mes == 2:
                                             print("Aquario")
                                         else:
-                                            if dia >= 19 and mes == "2" or dia <= 20 and mes == "3":
+                                            if dia >= 19 and mes == 2 or dia <= 20 and mes == 3:
                                                 print("Peixes")
                                             else:
                                                 print("Erro")
